254eb57bb72632122d5f9f3d9a29670eda0f8123
bbeale <ben.beale@medhub.com>
2019-06-26 09:13:22 -0400

    While testing with a known XSS vulnerability, I realized that it is hard to test individual form inputs if the code submits them all at once. Need to parse these and handle each one separately (keeping in mind required params, of course).

--- src/scanner.py
+++ src/scanner.py
@@ -31,6 +31,8 @@ class Scanner:
     def __init__(self, data):
 
         self.url            = data["url"]
+        self._test_payload  = '<img style="width:100px; height:100px" src="" onmouseover="javascript:alert(document.cookie.toString())" />'
+        # self._test_payload  = '" onmouseover="javascript:alert(1)" "'
 
         self.cookies        = None
         self.header         = None
@@ -38,7 +40,7 @@ class Scanner:
         self.referer        = None
         self.username       = None
         self.passwd         = None
-        self.ignore_links   = None
+        self.ignore_link    = None
 
         if "cookie" in data.keys():
             self.cookies = parse_cookies(cookie_list=data["cookie"].split(";"))
@@ -57,12 +59,13 @@ class Scanner:
             self.password = data["password"]
 
         if "ignore" in data.keys():
-            self.ignore_links = data["ignore"]
+            self.ignore_link = data["ignore"]
 
         self.session = requests.Session()
 
-        for cookie in self.cookies:
-            self.session.cookies.set(cookie[0], cookie[1])
+        if self.cookies:
+            for cookie in self.cookies:
+                self.session.cookies.set(cookie[0], cookie[1])
 
         self.links = []
 
@@ -73,7 +76,6 @@ class Scanner:
             "Accept-Language": "en-US,en;q=0.5",
             "Upgrade-Insecure-Requests": "1",
             "Connection": "keep-alive",
-            "DNT": "1",
         }
 
         if self.header is not None:
@@ -93,32 +95,45 @@ class Scanner:
     def extract_links(self, url):
         response = self.session.get(url)
         _links = []
-        html = BeautifulSoup(response.content.decode("utf-8"), features="html.parser")
+        html = BeautifulSoup(response.text, features="html.parser")
 
         elements = html.findAll(lambda tag: len(tag.attrs) > 0 and "href" in tag.attrs)
 
         for element in elements:
-            _links.append(element.attrs["href"])
+            if self.ignore_link is None:
+                _links.append(element.attrs["href"])
+                continue
+            elif self.ignore_link is not None and self.ignore_link not in element.attrs["href"]:
+                _links.append(element.attrs["href"])
+                continue
+            else:
+                continue
 
         links = []
         unables = []
         for link in _links:
             if url in link:
-                links.append(urlparse.urlparse(link))
+                links.append(link)
             else:
-                if link and link[0] is "/":
+                if link and link[0] == "/":
                     n_url = urlparse.urljoin(url, link)
-                    links.append(urlparse.urlparse(n_url))
+                    links.append(n_url)
+                    continue
+                elif link and link[0] != "/":
+                    n_url = urlparse.urljoin(url, link)
+                    links.append(n_url)
+                    continue
                 else:
-                    unables.append(urlparse.urlparse(link))
+                    unables.append(link)
 
         return links
 
     def extract_forms(self, url):
         response = self.session.get(url)
-        html = BeautifulSoup(response.content.decode("utf_8"), features="html.parser")
+        html = BeautifulSoup(response.text, features="html.parser")
         return html.findAll("form")
 
+    # TODO: Perhaps this should take an input instead of a whole form
     def submit(self, form, value, url):
         action = form.get("action")
         p_url = urlparse.urljoin(url, action)
@@ -127,20 +142,42 @@ class Scanner:
         inputs = form.findAll("input")
         p_data = dict()
 
+        responses = []
+        payload_set = False
         for i in inputs:
             i_name = i.get("name")
             i_type = i.get("type")
             i_value = i.get("value")
-            if i_type == "text":
-                i_value = value
 
             p_data[i_name] = i_value
 
+            if payload_set:
+                continue
+
+            if i_type == "text" or i_type == "hidden":
+                p_data[i_name] = value
+                payload_set = True
+
         if method == "post":
             response = self.session.post(p_url, data=p_data)
+
+
+
+
+
+            # TODO
+
+            if response.status_code == 302:
+                print("Request was redirected")
+
+            responses.append(response.text)
         else:
             response = self.session.get(p_url, params=p_data)
-        return response
+            if response.status_code == 302:
+                print("Request was redirected")
+
+            responses.append(response.text)
+        return responses
 
     def login(self, uname, pword, login_url=None):
 
@@ -154,7 +191,6 @@ class Scanner:
         return self.session.post(urlparse.urljoin(self.url, login_url), data=l_data)
 
     def crawl(self, url=None):
-        print('.')
         if url is None:
             url = self.url
 
@@ -169,24 +205,26 @@ class Scanner:
             if link not in self.links:
                 self.links.append(link)
                 self.crawl(link)
-        print('...')
 
     def scan(self, url=None):
         if url is None:
             url = self.url
 
+        if url not in self.links:
+            self.links.append(url)
+
         for link in self.links:
             forms = self.extract_forms(link)
             for form in forms:
                 print("[+] Testing forms in {}".format(link))
-                if self.form_xss_test(form, url):
+                if self.form_xss_test(form, link):
                     res = "\n\n[***] XSS found in {} in form {}".format(link, form)
                     self.scan_results.append(res)
                     print(res)
 
             if "=" in link:
                 print("[+] Testing link: {}".format(link))
-                if self.link_xss_test(url):
+                if self.link_xss_test(link):
                     res = "\n\n[***] XSS found in {}".format(link)
                     self.scan_results.append(res)
                     print(res)
@@ -195,19 +233,19 @@ class Scanner:
         if url is None:
             url = self.url
         if payload is None:
-            payload = "<script>alert(\"boo\")</script>"
+            payload = self._test_payload
         url = url.replace("=", "={}".format(payload))
         response = self.session.get(url)
-        return payload in response.content.decode("utf-8")
+        return payload in response.text
 
     def form_xss_test(self, form, url, payload=None):
         if url is None:
             url = self.url
-
+        # todo: I think I am going to want to parse the form inputs here instead, so I can test them one at a time
         if payload is None:
-            payload = "<script>alert(\"boo\")</script>"
-        response = self.submit(form, payload, url)
-        return payload in response.content.decode("utf-8")
+            payload = self._test_payload
+        responses = self.submit(form, payload, url)
+        return True in [payload in response.text for response in responses]
 
     def print_results(self):
-        print("Not yet implemented")
+        print("--Not yet implemented --\n\nScan results: ", self.scan_results)
